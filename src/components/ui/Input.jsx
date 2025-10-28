46b0dde542aa026470a91773da04c9d33e4978a7
GodlikeShimmyer <iggomak1@gmail.com>
2025-10-28 11:56:38 -0400

    Update Input.jsx

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import { clsx } from 'clsx';
+import React from "react";
+import clsx from "clsx";
 
-export function Input({ className = '', ...props }) {
+export default function Input({ placeholder = "", value, onChange, className = "" }) {
   return (
     <input
+      type="text"
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
       className={clsx(
-        'w-full px-3 py-2 bg-white/10 border border-white/20 rounded-lg text-white placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-godlike-green',
+        "px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500 w-full",
         className
       )}
-      {...props}
     />
   );
 }
